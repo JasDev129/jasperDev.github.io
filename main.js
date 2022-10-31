7077c33c6eb37cf31d60bfd7425944dfe25b44bb
xyper <xyper.gmail>
2022-10-31 22:11:05 +0800

    making it real

--- main.js
+++ main.js
@@ -23,40 +23,6 @@ let navHide = (navList.style.left = "-100%");
     });
 
 // DARK_MODE
-
-// let dark = false;
-// let bgLight = document.querySelector('.bg-light');
-//     bgLight.style.opacity = '0';
-// let bgdark = document.querySelector(".bg-dark");
-// const main = document.querySelector(".main");
-
-
-//     const darkMode = document.querySelector(".dark-mode")
-//     darkMode.addEventListener("click", () => {
-        
-//             if(dark == false){
-//                 main.style.backgroundColor = "#333333";
-//                 main.style.opacity = "1";
-//                 bgdark.style.left = '-15%'
-//                 bgdark.style.opacity = "0";
-//                 bgLight.style.opacity = '1';
-//                 bgLight.style.left = "-63%";
-//                 bgLight.style.color = "#eeee";
-//                 darkMode.style.border = '1px solid #eeee'
-//                 dark = true;
-//             }else{
-//                 main.style.opacity = "1";
-//                 main.style.backgroundColor = "#eeee";
-//                 bgLight.style.left = "0";
-//                 bgdark.style.left = "0%";
-//                 bgLight.style.opacity = "0";
-//                 bgdark.style.opacity = "1";
-//                 darkMode.style.border = "1px solid #333333";
-//                 dark = false;
-//             }
-//         }
-//       );
-
     const darkMode = document.getElementById('dark-mode');
     const moonIcon = document.querySelector(".bg-dark");
     const sunIcon = document.querySelector(".bg-light");
